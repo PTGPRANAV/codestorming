fd55ed8afdbb15b8fad5a0cfa18b88f4c7c3a067
PTGPRANAV <pranavdaundkar13@gmail.com>
2021-11-20 23:57:53 +0530

    first coding lesson

---
+++ q1.py
@@ -0,0 +1,7 @@
+rev_table = dict()
+for i in range(len(nums)):
+        second_addend = target - nums[i]
+        if second_addend in rev_table:
+        	return [rev_table[second_addend], i]
+        else:
+        	rev_table[nums[i]] = i
